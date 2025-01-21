363dc44880e8428e13e25a4ac52bb4738d468f39
Ijuaa <pierre.colpart@gmail.com>
2025-01-21 15:16:15 +0100

    fin du challenge

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
+<?php
+$errors = [];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $contact = array_map("trim", $_POST);
+    if (empty($contact['userName'])) {
+        $errors[] = 'Name is required';
+    }
+    if (empty($contact['userMail'])) {
+        $errors[] = 'Email is required';
+    } elseif (!filter_var($contact['userMail'], FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'L\'email n\'est pas valide';
+    }
+
+    if (empty($contact['subject'])) {
+        $errors[] = 'Please choose a subject in the list.';
+    }
+    if (empty($errors)) {
+        header('Location: validation.php');
+        exit();
+    }
+    // if (empty($contact['message'])) {
+    //     $errors[] = 'Please enter a message';
+    // }
+    // if (strlen($contact['message']) <= 10) {
+    //     $errors[] = 'Your message must be at least 10 caracters long.';
+    // }
+
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,7 +37,9 @@
     <link rel="stylesheet" href="/assets/styles/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap"
+        rel="stylesheet">
 </head>
 
 <body>
@@ -55,7 +86,41 @@
                 quaerat nemo nam, consequuntur nisi alias in praesentium. Fuga amet esse nam doloremque ut nemo nostrum.
             </p>
         </section>
-        <?php //@todo Add a contact form  ?>
+        <section class="formContainer container">
+            <h2 id="contact">Get in touch</h2>
+            <?php if (count($errors) > 0): ?>
+                <h3>Please fix errors below</h3>
+                <ul>
+                    <?php foreach ($errors as $error): ?>
+                        <li><?= $error ?></li>
+                    <?php endforeach ?>
+                </ul>
+            <?php endif ?>
+            <form id="contactForm" action="" method="post" novalidate>
+                <label for="userName">Name <span class="required">*</span></label>
+                <input required type="text" name="userName" id="userName" value="<?= $contact['userName'] ?? '' ?>">
+
+                <label for="userMail">Email <span class="required">*</span></label>
+                <input required type="email" name="userMail" id="userMail" value="<?= $contact['userMail'] ?? '' ?>">
+
+                <label for="subject">Subject <span class="required">*</span></label>
+                <select required name="subject" id="subject">
+                    <option value="" disabled <?= empty($contact['subject']) ? 'selected' : '' ?>>Select a subject
+                    </option>
+                    <option value="rendez-vous" <?= (isset($contact['subject']) && $contact['subject'] === 'rendez-vous') ? 'selected' : '' ?>>Make an appointment</option>
+                    <option value="newsletter" <?= (isset($contact['subject']) && $contact['subject'] === 'newsletter') ? 'selected' : '' ?>>Subscribe to the newsletter</option>
+                    <option value="reclamation" <?= (isset($contact['subject']) && $contact['subject'] === 'reclamation') ? 'selected' : '' ?>>Reclamation</option>
+                    <option value="quotation" <?= (isset($contact['subject']) && $contact['subject'] === 'quotation') ? 'selected' : '' ?>>Ask for a quotation</option>
+                </select>
+
+
+                <label for="userMessage">Message</label>
+                <textarea name="userMessage" id="userMessage"
+                    rows="5"><?= htmlspecialchars($contact['userMessage'] ?? '') ?></textarea>
+
+            </form>
+            <button class="formButton" form="contactForm">SEND</button>
+        </section>
     </main>
     <?php include '_footer.php' ?>
 </body>
---
+++ validation.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validation</title>
+    <link rel="stylesheet" href="/assets/styles/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+
+    <?php include('_navbar.php') ?>
+
+    <main class="container">
+        <h1 class="thanks">Thank you for your message!</h1>
+        <div class="validationMessage">
+            <p>Your message is being processed. </br>
+                Our Team will get back to you within 48 hours.
+            </p>
+            <p>Have a nice day and see you soon 🙂</p>
+        </div>
+    </main>
+    <?php include '_footer.php' ?>
+</body>
+
+</html>
